0a78773aaeb640f045b5b74ef97d22719aa83e33
gecmanow <gecmanow@gmail.com>
2022-02-01 12:49:08 +0700

    edit

--- js/script.js
+++ js/script.js
@@ -20,3 +20,32 @@ let elements = document.querySelectorAll('.animation');
 for (let elm of elements) {
     observer.observe(elm);
 }
+
+let cycleItem = 0;
+
+function cycleElems() {
+    $('.projects__client-property').hide();
+    $('.projects__client-property').css('opacity', '0');
+    $('.client-item-' + cycleItem).show();
+    $('.client-item-' + cycleItem).css('opacity', '1');
+    
+    let maxItem = 2;
+    cycleItem++;
+    if(cycleItem > maxItem) {
+        cycleItem = 0;
+    }
+}
+document.addEventListener("DOMContentLoaded", function() {
+    setInterval(cycleElems, 2000);
+});
+
+document.addEventListener("scroll", function() {
+    let header = document.querySelector('.header');
+    console.log(scrollY);
+
+    if(scrollY > 1) {
+        header.classList.add('header-pc-sticky');
+    } else {
+        header.classList.remove('header-pc-sticky');
+    }
+})
